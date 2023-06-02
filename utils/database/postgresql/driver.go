8590aa5fcfb47f77b9ac6bb801bfb76867a9f221
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-06-02 15:35:25 +0700

    add: db Postgres

--- utils/database/postgresql/driver.go
+++ utils/database/postgresql/driver.go
@@ -1,1 +1,19 @@
 package postgresql
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/iqbalpradipta/BalPay/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func initDB(cfg *config.AppConfig) *gorm.DB {
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", cfg.DB_USERNAME, cfg.DB_PASSWORD, cfg.DB_HOST, cfg.DB_PORT, cfg.DB_NAME)
+	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		log.Fatal("error connect to DB", err)
+	}
+	return db
+}
