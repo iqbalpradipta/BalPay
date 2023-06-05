9e7066303c7a3c4e9029b6cf6a05368d5581355d
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-06-05 21:47:28 +0700

    add feature user

--- features/user/delivery/handler.go
+++ features/user/delivery/handler.go
@@ -2,6 +2,7 @@ package delivery
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/iqbalpradipta/BalPay/features/user"
 	"github.com/iqbalpradipta/BalPay/middlewares"
@@ -19,13 +20,52 @@ func NewHandler(e *echo.Echo, useCase user.IusecaseInterface) {
 	}
 
 	e.GET("/user", handler.GetUser, middlewares.JWTMiddleware(), middlewares.IsAdmin)
-	
+
 }
 
 func (d *UserDelivery) GetUser(c echo.Context) error {
 	result, err := d.UserUsecase.GetUser()
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, helper.FailedResponseHelper("failed to get data"))
+		return c.JSON(http.StatusBadRequest, helper.FailedResponseHelper("failed to get data"))
 	}
 	return c.JSON(http.StatusOK, helper.SuccessResponseDataHelper("success get data", fromCoreList(result)))
 }
+
+
+func (d *UserDelivery) GetUserById(c echo.Context) error {
+	id := c.Param("id")
+	idConv, errConv := strconv.Atoi(id)
+	if errConv != nil {
+		return c.JSON(http.StatusBadRequest, helper.FailedResponseHelper("error param"))
+	}
+	result, err := d.UserUsecase.GetUserById(idConv)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, helper.FailedResponseHelper("error get data"))
+	}
+	return c.JSON(http.StatusOK, helper.SuccessResponseDataHelper("get data success", fromCoreToResponse(result)))
+}
+
+func (d *UserDelivery) CreateData(c echo.Context) error {
+	var dataRequest UserRequest
+	errBind := c.Bind(&dataRequest)
+	if errBind != nil {
+		return c.JSON(http.StatusBadRequest, helper.FailedResponseHelper("error bind data"))
+	}
+	row, err := d.UserUsecase.CreateData(toCore(dataRequest))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError,helper.FailedResponseHelper("error insert data"))
+	}
+	if row != 1 {
+		return c.JSON(http.StatusInternalServerError, helper.FailedResponseHelper("error insert data"))
+	}
+	return c.JSON(http.StatusOK, helper.SuccessResponseHelper("success create data"))
+}
+
+// func (d *UserDelivery) UpdateData(c echo.Context) error {
+// 	id := c.Param("id")
+// 	idConv, errConv := strconv.Atoi(id)
+// 	if errConv != nil {
+// 		return c.JSON(http.StatusBadRequest, helper.FailedResponseHelper("error param"))
+// 	}
+// 	idToken := middlewares.ExtractToken(c)
+// }
--- features/user/delivery/request.go
+++ features/user/delivery/request.go
@@ -1,8 +1,28 @@
 package delivery
 
+import "github.com/iqbalpradipta/BalPay/features/user"
+
 type UserRequest struct {
 	Name		string	`json:"name" form:"name"`
 	Username	string	`json:"username" form:"username"`
 	Email		string	`json:"email" form:"email"`
 	Password	string	`json:"password" form:"password"`
+}
+
+func fromCoreRequest(dataCore UserRequest) user.Core {
+	return user.Core{
+		Name: dataCore.Name,
+		Username: dataCore.Name,
+		Email: dataCore.Email,
+		Password: dataCore.Password,
+	}
+}
+
+func toCore(data UserRequest) user.Core {
+	return user.Core{
+		Name: data.Name,
+		Username: data.Username,
+		Email: data.Email,
+		Password: data.Password,
+	}
 }
