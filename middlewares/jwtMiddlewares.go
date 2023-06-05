3cb58f685f73dadd172796ebfc4c8be519bc2bef
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-06-05 07:52:05 +0700

    Fix

--- middlewares/jwtMiddlewares.go
+++ middlewares/jwtMiddlewares.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/iqbalpradipta/BalPay/config"
-
 	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -14,7 +13,7 @@ import (
 func JWTMiddleware() echo.MiddlewareFunc {
 	return middleware.JWTWithConfig(middleware.JWTConfig{
 		SigningMethod: "HS256",
-		SigningKey: []byte(config.SECRET_JWT),
+		SigningKey:    []byte(config.SECRET_JWT),
 	})
 }
 
