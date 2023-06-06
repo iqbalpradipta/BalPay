c687252c9de2c02ee1b279ebe7236b0650b7b947
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-06-06 11:05:10 +0700

    add: migrate

--- migration/migrate.go
+++ migration/migrate.go
@@ -1,2 +1,12 @@
 package migration
 
+import (
+	"github.com/iqbalpradipta/BalPay/features/user/data"
+	"gorm.io/gorm"
+)
+
+func InitMigrate(db *gorm.DB)  {
+	db.AutoMigrate(
+		new(data.User),
+	)
+}
