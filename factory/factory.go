6baaeaad5685552a861b2e8dace05c7fd627cf59
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-06-06 11:04:37 +0700

    add: factory

--- factory/factory.go
+++ factory/factory.go
@@ -3,8 +3,14 @@ package factory
 import (
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
+
+	userData "github.com/iqbalpradipta/BalPay/features/user/data"
+	deliveryUser "github.com/iqbalpradipta/BalPay/features/user/delivery"
+	useCaseUser "github.com/iqbalpradipta/BalPay/features/user/usecase"
 )
 
 func InitFactory(e *echo.Echo, db *gorm.DB)  {
-	
+	userDataFactory := userData.New(db)
+	userUsecaseFactory := useCaseUser.NewLogic(userDataFactory)
+	deliveryUser.NewHandler(e, userUsecaseFactory)
 }
